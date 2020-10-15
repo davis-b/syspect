337ba68157aeb25c11079491d2699d61462c4885
Davis <obviouspy@gmail.com>
2020-10-15 10:22:03 -0400

    Clean up build.zig

--- build.zig
+++ build.zig
@@ -16,22 +16,9 @@ pub fn build(b: *Builder) void {
         exe.linkLibC();
         exe.install();
     }
-    //     const exe = b.addExecutable("redirector", "src/examples/connect_redirector.zig");
-    //     exe.addPackagePath("syspect", "src/index.zig");
-    //     exe.setBuildMode(mode);
-    //     exe.linkLibC();
-    //     exe.install();
 
-    //    const run_cmd = exe.run();
-    //    run_cmd.step.dependOn(b.getInstallStep());
-    //
-    //    const run_step = b.step("run", "Run the app");
-    //    run_step.dependOn(&run_cmd.step);
-
-    const test_step = b.step("test", "Run library tests");
-
-    var test_main = b.addTest("src/main.zig");
-    // test_main.addPackagePath("maker", "src/maker.zig");
-    test_main.setBuildMode(mode);
-    test_step.dependOn(&test_main.step);
+    // const test_step = b.step("test", "Run library tests");
+    // var test_main = b.addTest("src/main.zig");
+    // test_main.setBuildMode(mode);
+    // test_step.dependOn(&test_main.step);
 }
