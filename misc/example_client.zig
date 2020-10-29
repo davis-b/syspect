6f5042c8dff9bfdfa8135d760c09a14f6b9b2066
Davis <obviouspy@gmail.com>
2020-10-29 04:04:35 -0400

    Print when thread's work is done

--- misc/example_client.zig
+++ misc/example_client.zig
@@ -5,10 +5,11 @@ pub fn main() !void {
     const ip = "127.0.0.1";
     connect(.{ .ip = ip, .port = 9885 });
     var ctx = Ctx{ .ip = ip, .port = 9887 };
-    for ([_]u1{0} ** 3) |_| {
+    for ([_]u1{0} ** 3) |_, index| {
         ctx.port += 1;
         var t = try std.Thread.spawn(ctx, connect);
         t.wait();
+        std.debug.warn("thread {} done!\n", .{index});
     }
 }
 
